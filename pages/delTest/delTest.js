2edb3d1545263c26df38b713d7a5e2f04f50a684
linyijin <lyj183@yahoo.com>
2019-05-06 17:40:58 +0800

    删除cell功能

--- pages/delTest/delTest.js
+++ pages/delTest/delTest.js
@@ -5,24 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    items: [],
-    startX: 0, //开始坐标
-    startY: 0
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this;
-    for (var i = 0; i < 10; i++) {
-      this.data.items.push({
-        content: i + " 向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦,向左滑动删除哦",
-        isTouchMove: false //默认隐藏删除
-      })
+    for (var i = 0; i < 20; i++) {
+      var msg = {};
+      msg.msgText = '沪D086X' + i;
+      msg.id = i;
+      msg.status = true //true为正常显示，false为显示删除按钮
+      this.data.list.push(msg);
     }
-    this.setData({
-      items: this.data.items
+    this.setData({ 
+      list: this.data.list,
     });
 
   },
@@ -76,71 +74,44 @@ Page({
 
   },
 
-  //手指触摸动作开始 记录起点X坐标
-  touchstart: function (e) {
-    //开始触摸时 重置所有删除
-    this.data.items.forEach(function (v, i) {
-      if (v.isTouchMove)//只操作为true的
-        v.isTouchMove = false;
-    })
+  touchS(e) {
+    // 获得起始坐标
+    this.startX = e.touches[0].clientX;
+    this.startY = e.touches[0].clientY;
+  }, 
 
-    this.setData({
-      startX: e.changedTouches[0].clientX,
-      startY: e.changedTouches[0].clientY,
-      items: this.data.items
-    })
-
-  },
-
-  //滑动事件处理
-  touchmove: function (e) {
-    var that = this,
-      index = e.currentTarget.dataset.index,//当前索引
-      startX = that.data.startX,//开始X坐标
-      startY = that.data.startY,//开始Y坐标
-      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
-      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
-      //获取滑动角度
-      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
-    that.data.items.forEach(function (v, i) {
-      v.isTouchMove = false
-      //滑动超过30度角 return
-      if (Math.abs(angle) > 30) return;
-      if (i == index) {
-        if (touchMoveX > startX) //右滑
-          v.isTouchMove = false
-        else //左滑
-          v.isTouchMove = true
+  touchM(e) {
+    // 获得当前坐标
+    this.currentX = e.touches[0].clientX;
+    this.currentY = e.touches[0].clientY;
+    const x = this.startX - this.currentX; //横向移动距离
+    const y = Math.abs(this.startY - this.currentY); //纵向移动距离，若向左移动有点倾斜也可以接受
+    if (x > 35 && y < 110) {
+      //向左滑是显示删除
+      for (var i = 0; i < this.data.list.length; i++) {
+        if (i != e.currentTarget.id) {
+          this.data.list[i].status = true;
+        } else {
+          this.data.list[i].status = false;
+        }
       }
-
-    })
-
-    //更新数据
-    that.setData({
-      items: that.data.items
-    })
-
+      this.setData({
+        list: this.data.list
+      })
+    } else if (x < -35 && y < 110) {
+      //向右滑
+      this.data.list[e.currentTarget.id].status = true;
+      this.setData({
+        list: this.data.list
+      })
+    }
   },
-
-  /**
-  * 计算滑动角度
-  * @param {Object} start 起点坐标
-  * @param {Object} end 终点坐标
-  */
-
-  angle: function (start, end) {
-    var _X = end.X - start.X,
-      _Y = end.Y - start.Y
-    //返回角度 /Math.atan()返回数字的反正切值
-    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-  },
-
-  //删除事件
-  del: function (e) {
-    this.data.items.splice(e.currentTarget.dataset.index, 1)
+  delete(e) {
+    this.data.list.splice(e.currentTarget.id, 1)
     this.setData({
-      items: this.data.items
+      list: this.data.list
     })
   }
+
 
 })
